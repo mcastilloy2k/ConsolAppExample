f8bf7b36d417cd89cbe7884aef4483499a92e218
Marvin Castillo <mcastilloy2k@gmail.com>
2018-04-17 22:45:13 -0600

    Add project files.

---
+++ ConsoleAppExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleAppExample
+{
+    class Program
+    {
+        [DllImport(@"C:\Users\Marvin Castillo\source\repos\ExampleForDLL\x64\Debug\ExampleForDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Add(double a, double b);
+        [DllImport(@"C:\Users\Marvin Castillo\source\repos\ExampleForDLL\x64\Debug\ExampleForDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Multiply(double a, double b);
+        [DllImport(@"C:\Users\Marvin Castillo\source\repos\ExampleForDLL\x64\Debug\ExampleForDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double AddMultiply(double a, double b);
+        [DllImport(@"C:\Users\Marvin Castillo\source\repos\ExampleForDLL\x64\Debug\ExampleForDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Count(string text);
+        static void Main(string[] args)
+        {
+            var text = "1234567890";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Add: {Add(7, 7)}");
+            Console.WriteLine($"Multiply: {Multiply(4, 4)}");
+            Console.WriteLine($"AddMultiply: {AddMultiply(3, 3)}");
+            Console.WriteLine($"Lenght: {Count(text)}");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
